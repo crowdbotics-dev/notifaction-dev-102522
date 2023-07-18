ec39112571e8afa571c0dbc631fe26227f662872
Crowdbotics <engineering@crowdbotics.com>
2023-07-18 09:25:55 +0000

    Initial commit.

---
+++ backend/notifaction_dev_102522/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for notifaction_dev_102522 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notifaction_dev_102522.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "notifaction-dev-102522",
+  url: "https://notifaction-dev-102522.botics.co",
+  api: "https://notifaction-dev-102522.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
