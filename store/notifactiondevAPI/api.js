bfcbe4fa4c2074671c3367934d41c9167722192f
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-07-19 00:29:55 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/notifactiondevAPI/api.js
+++ store/notifactiondevAPI/api.js
@@ -1,55 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const notifactiondevAPI = axios.create({
   baseURL: "https://notifaction-dev-102522.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return notifactiondevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return notifactiondevAPI.post(`/api/v1/login/`, payload)
+  return notifactiondevAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return notifactiondevAPI.post(`/api/v1/signup/`, payload)
+  return notifactiondevAPI.post(`/api/v1/signup/`, payload);
 }
+
 function rest_auth_login_create(payload) {
-  return notifactiondevAPI.post(`/rest-auth/login/`, payload)
+  return notifactiondevAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return notifactiondevAPI.get(`/rest-auth/logout/`)
+  return notifactiondevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return notifactiondevAPI.post(`/rest-auth/logout/`)
+  return notifactiondevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return notifactiondevAPI.post(`/rest-auth/password/change/`, payload)
+  return notifactiondevAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return notifactiondevAPI.post(`/rest-auth/password/reset/`, payload)
+  return notifactiondevAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return notifactiondevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return notifactiondevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return notifactiondevAPI.post(`/rest-auth/registration/`, payload)
+  return notifactiondevAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return notifactiondevAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload
-  )
+  return notifactiondevAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return notifactiondevAPI.get(`/rest-auth/user/`)
+  return notifactiondevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return notifactiondevAPI.put(`/rest-auth/user/`, payload)
+  return notifactiondevAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return notifactiondevAPI.patch(`/rest-auth/user/`, payload)
+  return notifactiondevAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -65,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
